d30d0f31a3b28df370f1f6e67676b9e093972620
Jae-Hyup Cho <Jae-Hyup.Cho+mlx@metrolinx.com>
2026-05-13 19:02:27 -0600

    Initial Commit

---
+++ titanic.py
@@ -0,0 +1,35 @@
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+from IPython.display import Image
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import warnings
+
+warnings.filterwarnings('ignore')
+df = sns.load_dataset('titanic')
+
+df.head()
+
+
+def transform_who(x):
+    if x == 'man':
+        return '남자'
+    elif x == 'woman':
+        return '여자'
+    else:
+        return '아이'
+
+
+#df['who'] = df['who'].apply(transform_who)
+
+def transform_fare(x):
+    return x['fare'] / x['age']
+
+df['who'] = df.apply(transform_fare, axis=1)
+
+
+
+
+
